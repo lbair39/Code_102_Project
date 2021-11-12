2c6e9f450d08e29dcdc185d051edd160319ef2d2
lbair39 <lilybair24@g.crookcountyschools.org>
2021-11-12 10:07:11 -0800

    Worked on JS

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 // console.log('I am working')
 
-document.write("<h1>Welcome to our Interactive Story Game!</h1>")
+alert('Answer the following questions.' )
+
+let userName = prompt('What is your name?')
+let question1 = prompt('Are you a male or female? Please answer with "Male" or "Female".')
+let question2 = prompt('What is your favorite color?')
+let question3 = prompt('Do you like hiking? Please use "Yes" or "No".')
+let question4 = prompt('Would you decribe hiking as Calming or Boring?')
+let question5 = prompt('If you were hiking would you follow the path made for you or would you go your own way? Answer with "Path" or "Own Way".')
+let question6 = prompt('If you were hiking and you came across a bear, would you fight or flight? Answer with either "Fight" or "Run Away".')
+
+document.write('<h1>Hello ' + userName + ', welcome to our interactive story game!</h1>')
+
+document.write('Your responses to the questions you just entered was used to show how your evening hike will go')
