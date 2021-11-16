4b7fb7a90c4f0eeed70de2cf8d04e32d1d661ff8
lbair39 <lilybair24@g.crookcountyschools.org>
2021-11-16 10:03:18 -0800

    added more to js

--- script.js
+++ script.js
@@ -11,15 +11,22 @@ document.write('<h1>Hello ' + userName + ', welcome to our interactive story gam
 
 document.write('<h2>Your response to the questions was used to show how your hike will go.</h2>')
 if(question2 === "No"){
-    question2answer = ('<h3>Your parents forced you to go on a hike because you were being too lazy at home, you had chose to wear your favorite ' + question1 + ' jacket. </h3>')
+    question2answer = ('<h3>Your parents forced you to go on a hike because you were being too lazy at home.</h3>')
 }
 if(question2 === "Yes"){
-    document.write('<h3>You had decided to go on a hike because you needed to clear your mind, you had chose to wear your favorite ' + question1 + ' jacket. </h3>')
-}
-
-if(question3 === "No"){
-    document.write('<h3></h3>')
+    question2answer = ('<h3>You had decided to go on a hike because you needed to clear your mind.</h3>')
 }
 if(question3 === "Yes"){
-    document.write('<h3></h3>')
+    question3answer = ('<h3>You noticed there was an opening on the side of the path, you decided to stay on the path made for you. It started to rain and you relized the path was getting extremely hard to walk on. You looked around for shelter or a tree to sit under. You saw that there was a cave.</he>')
 }
+if(question3 === "No"){
+    question3answer = ('<h3>You noticed there was an opening on the side of the path, you decided to see where it would lead you. While walking down this opening you came across a cave. You noticed it was starting to rain so you went into the cave for shelter</h>')
+}
+if(question4 === "Run"){
+    question4answer = ('<h3>While inside of the cave, you heard something moving. You walked deeper into the cave to see what it was. Oh no, its a bear! You decided to try and run away from it. The bear chases you out of the cave but you end up slipping due to the wet ground. The bear mauled you and fed you to her cubs. </h3>')
+}
+if(question4 === "Challenge"){
+    question4answer = ('<h3>While inside of the cave, you heard something moving. You walked deeper into the cave to see what it was. Oh no, its a bear! You decided to challenge the bear. While throwing around some karate moves, the bear felt very threatened. She and her cubs ran out of the cave.</h3>')
+}
+
+document.write('<p>You threw on your favorite ' + question1 + ' jacket. ' + question2answer + ' The hike was very calming. ' + question3answer + '.</p>')
