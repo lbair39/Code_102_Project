506cdf74d343ae2c8cf44fa242b5fd4722c7ddd5
lbair39 <lilybair24@g.crookcountyschools.org>
2021-11-17 10:00:05 -0800

    Created the story

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 alert('Answer the following questions.' )
 
 let userName = prompt('What is your name?')
+let petsName = prompt('Whats the name of your dog? If you dont have one, come up with a name.')
 let question1 = prompt('What is your favorite color?')
 let question2 = prompt('Do you like hiking? Please use "Yes" or "No"')
 let question3 = prompt('Will you stay on the path created for you? Use "Yes" or "No".')
@@ -11,22 +12,22 @@ document.write('<h1>Hello ' + userName + ', welcome to our interactive story gam
 
 document.write('<h2>Your response to the questions was used to show how your hike will go.</h2>')
 if(question2 === "No"){
-    question2answer = ('<h3>Your parents forced you to go on a hike because you were being too lazy at home.</h3>')
+    question2answer = ('<h3>Your parents forced you to go on a hike because you were being too lazy at home. </h3>')
 }
 if(question2 === "Yes"){
-    question2answer = ('<h3>You had decided to go on a hike because you needed to clear your mind.</h3>')
+    question2answer = ('<h3>You had decided to go on a hike because you needed to clear your mind. </h3>')
 }
 if(question3 === "Yes"){
-    question3answer = ('<h3>You noticed there was an opening on the side of the path, you decided to stay on the path made for you. It started to rain and you relized the path was getting extremely hard to walk on. You looked around for shelter or a tree to sit under. You saw that there was a cave.</he>')
+    question3answer = ('<h3>You noticed there was an opening on the side of the path, you decided to stay on the path made for you because it was safer. It started to rain and you realized the path was getting extremely hard to walk on. You looked around for shelter or a tree to sit under with your dog. You saw that there was a cave so you decided to go inside to escape the rain. </he>')
 }
 if(question3 === "No"){
-    question3answer = ('<h3>You noticed there was an opening on the side of the path, you decided to see where it would lead you. While walking down this opening you came across a cave. You noticed it was starting to rain so you went into the cave for shelter</h>')
+    question3answer = ('<h3>You noticed there was an opening on the side of the path, you wanted to see where it would lead you. While walking down this opening you came across a cave. You noticed it was starting to rain and your dog was getting muddy, so you went into the cave for shelter. </h3>')
 }
 if(question4 === "Run"){
-    question4answer = ('<h3>While inside of the cave, you heard something moving. You walked deeper into the cave to see what it was. Oh no, its a bear! You decided to try and run away from it. The bear chases you out of the cave but you end up slipping due to the wet ground. The bear mauled you and fed you to her cubs. </h3>')
+    question4answer = ('<h3>You chose to try and run away from it. The bear chases you out of the cave but you end up slipping due to the wet ground, you looked around for your dog and he was no where to be seen. You had thought that he got eaten by the bear. The bear was hovering over you getting ready to feed you to her cubs. In the corner of your eye you see ' + petsName + '. Your dog communicated with the bear making a peace offering. You and your dog ended up leaving the cave and sat under a random tree. After it stopped raining you guys went home and made it there safely. You now have a fear of bears and caves. </h3>')
 }
 if(question4 === "Challenge"){
-    question4answer = ('<h3>While inside of the cave, you heard something moving. You walked deeper into the cave to see what it was. Oh no, its a bear! You decided to challenge the bear. While throwing around some karate moves, the bear felt very threatened. She and her cubs ran out of the cave.</h3>')
+    question4answer = ('<h3>You chose to challenge the bear. While throwing around some karate moves and your dog barking, the bear felt very threatened. She and her cubs ran out of the cave into the rain. You were suprised you had survived and made it home safely. <h3>')
 }
 
-document.write('<p>You threw on your favorite ' + question1 + ' jacket. ' + question2answer + ' The hike was very calming. ' + question3answer + '.</p>')
+document.write('<p>You threw on your favorite ' + question1 + ' jacket and grabbed your dog ' + petsName + '.' + question2answer + ' The hike was very calming. ' + question3answer + 'It was very cold. While inside of the cave, you heard something moving. You walked deeper into the cave to see what it was. Oh no, its a bear!' + question4answer +'</p>')
